d9b665ec659172bb6e3c97d2e96f369e22eeb676
pariharbhavesh20@gmail.com <pariharbhavesh20@gmail.com>
2026-05-01 13:48:18 +0530

    changed filename to lowercase

--- frontend/src/pages/projects.jsx
+++ frontend/src/pages/projects.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navbar";
 import CreateProjectModal from "../components/createProjectModal";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
